673f47027f728026df7216f01e3461061937b1b5
awb99 <andreas.wolfgang.bauer@gmail.com>
2020-03-31 20:54:57 -0500

    lein ancient + data.csv dep update

--- project.clj
+++ project.clj
@@ -10,13 +10,14 @@
    ;[tick "0.4.17-alpha"] ; replacement for clj-time
    [cheshire "5.8.1"] ; JSON encoding
    [clj-http "3.10.0"]  ; http requests (bybit)                        
-   [org.clojure/data.csv "0.1.4"] ; read/write csv
+   [org.clojure/data.csv "1.0.0"] ; read/write csv
    [net.cgrand/xforms "0.18.2"] ; transducers for timeseries (ema sma)
    [org.ta4j/ta4j-core "0.12"] ; ta4j java technical indicator library
    [org.pinkgorilla/throttler "1.0.1"] ; throtteling (custom version, core.async upgrade)
    ; [com.stuartsierra/frequencies "0.1.0"]     ; percentile stats
    
    ]
+  :plugins [[lein-ancient "0.6.15"]]
   :repl-options {:init-ns ta.model.single}
   :source-paths ["src" "dev"]
   :resource-paths ["resources"])
