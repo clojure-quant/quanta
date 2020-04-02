4d822bf96f1d98f2ecdda2dc4c55633ab06c4b69
CI/CD <ci@pinkgorilla.org>
2020-04-02 03:57:40 +0000

    Release 0.1.7

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.pinkgorilla/ta "0.1.7-SNAPSHOT"
+(defproject org.pinkgorilla/ta "0.1.7"
   :license {:name "MIT"}
   :deploy-repositories [["releases" {:url "https://clojars.org/repo"
                                      :username :env/release_username
