29683c9a88b396dd7e0020c2fd65c1354cb1b85b
awb99 <andreas.wolfgang.bauer@gmail.com>
2020-04-23 21:22:08 -0500

    python moved to python-gorilla

--- profiles/notebook/src/notebook/main.clj
+++ profiles/notebook/src/notebook/main.clj
@@ -1,16 +1,8 @@
 (ns notebook.main
   (:require
-   ;[clojure.core.async :refer [thread]]
-   ;[dataset-tools.core :as dt] crashes all. opened ticket
-   ;[pinkgorilla.notebook-app.cli :refer [parse-opts]]
-   ;[pinkgorilla.notebook-app.core :refer [run-gorilla-server]]
    [pinkgorilla.embedded :refer [run-notebook #_start-notebook]]
    [gigasquid.utils]) ; bring to jar
   (:gen-class))
-
-;; the notebook secret management 
-;; currently expects ./test/creds.edn file
-;; for security, this file is excluded from git
 
 (defn start []
   (run-notebook "./profiles/notebook/config.edn"))
