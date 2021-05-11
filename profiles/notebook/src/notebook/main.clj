ac3abadfb46e6b80315115d19a44202d8407029f
awb99 <andreas.wolfgang.bauer@gmail.com>
2021-05-10 22:41:15 -0500

    notebook-bundel 0.5

--- profiles/notebook/src/notebook/main.clj
+++ profiles/notebook/src/notebook/main.clj
@@ -1,18 +1,34 @@
 (ns notebook.main
   (:require
-   [pinkgorilla.embedded :refer [run-notebook #_start-notebook]]
-   [gigasquid.utils]) ; bring to jar
+   [pinkgorilla.notebook-ui.app-bundel.app :refer [notebook-bundel-run!]]
+   [gigasquid.utils]; bring to jar
+   )
   (:gen-class))
 
-(defn start []
-  (run-notebook "./profiles/notebook/config.edn"))
+
+(def notebook-config
+  {:explorer {:client {:repositories [{:name "local"  :url "/api/explorer" :save true}
+                                      #_{:name "public" :url "https://raw.githubusercontent.com/pink-gorilla/gorilla-explore/master/resources/list.json"}]}
+              :server {:resource-root-path "notebooks-bad"
+                       :roots  
+                       {:ta "./notebooks"
+                        :gorilla-plot "../../gorilla/gorilla-plot/notebooks"
+                        ;:backtest "../../quant/backtest/notebooks"
+                        ;:fquant "../../quant/clojureQuant/notebooks"
+                        ;:gorilla-ui "../gorilla-ui/resources/notebooks"
+                        }}}
+   
+})
+
+(defn start-notebook! []
+  (notebook-bundel-run! notebook-config))
 
 (defn -main []
   (println "Running PinkGorilla Notebook")
-  (start))
+  (start-notebook!))
 
 (comment
-  (run-notebook)
+  (start-notebook!)
 
   ;comment end
   )
