def1bdad31ad970712b41d3b84b63723bbed7edd
awb99 <andreas.wolfgang.bauer@gmail.com>
2021-07-11 01:41:53 -0500

    aggrid with date

--- profiles/trateg/src/ta/backtest/table.clj
+++ profiles/trateg/src/ta/backtest/table.clj
@@ -3,7 +3,8 @@
    [tech.v3.dataset :as tds]))
 
 (defn ds->table [ds]
-  (let [ds-safe (dissoc ds :date)
+  (let [;ds-safe (dissoc ds :date)
+        ds-safe ds
         data (into [] (tds/mapseq-reader ds-safe))]
     data))
 
