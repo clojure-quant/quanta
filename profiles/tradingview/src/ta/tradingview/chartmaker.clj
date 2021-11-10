bd8ccdeea5553f89e67c345721aedfac608893ff
awb99 <andreas.wolfgang.bauer@gmail.com>
2021-11-10 23:57:27 +0100

    chartmaker gann working

--- profiles/tradingview/src/ta/tradingview/chartmaker.clj
+++ profiles/tradingview/src/ta/tradingview/chartmaker.clj
@@ -326,10 +326,10 @@
             {:time_t 1516113000, :offset 0, :price 1294.0812396146428}]})
 
 
-(def source-gann
+(defn source-gann [{:keys [symbol a-p b-p a-t b-t]}]
   {:type "LineToolGannComplex"
    :id "HirvcH"
-   :state {:symbol "NasdaqNM:AMZN"
+   :state {:symbol symbol
            :zorder 2
            :linkKey "7ezN7K4XBLS8"
            :ownerSource "pOQ6pA"
@@ -374,8 +374,68 @@
                     :3 [1 "rgba( 0, 155, 0, 1)" true]
                     :4 [1 "rgba( 0, 153, 101, 1)" true]
                     :5 [1 "rgba( 128, 128, 128, 1)" true]}}
-   :points [{:time_t 1511879400, :offset 0, :price 1517.5723703553008}
-            {:time_t 1515076200, :offset 0, :price 1794.9665041530848}]})
+   :points [{:time_t a-t, :offset 0, :price a-p }
+            {:time_t b-t, :offset 0, :price b-p}]})
+
+
+(defn source-gann [{:keys [symbol a-p b-p a-t b-t]}]
+{:type "LineToolGannComplex"
+ :id "Ix5dtc"
+ :state {:intervalsVisibilities {:minutesFrom 1 :daysTo 366  :secondsTo 59  :hoursTo 24  :months true  :days true  :seconds true  :daysFrom 1  :secondsFrom 1
+                                 :hours true :ranges true  :hoursFrom 1  :minutes true  :minutesTo 59  :weeks true}
+         :labelsStyle {:font "Verdana"
+                       :fontSize 12
+                       :bold false
+                       :italic false}
+         :arcsBackground {:fillBackground true
+                          :transparency 80}
+         :fillBackground false
+         :_isActualInterval true
+         :fanlines {:10 {:color "rgba( 165, 0, 255, 1)"  :visible false  :width 1  :x 1 :y 8}
+                    :0 {:color "rgba( 165, 0, 255, 1)" :visible false  :width 1 :x 8 :y 1}
+                    :4 {:color "rgba( 105, 158, 0, 1)" :visible true  :width 1  :x 2  :y 1}
+                    :7 {:color "rgba( 0, 153, 101, 1)" :visible false  :width 1  :x 1  :y 3}
+                    :1 {:color "rgba( 165, 0, 0, 1)" :visible false  :width 1  :x 5  :y 1}
+                    :8 {:color "rgba( 0, 0, 153, 1)" :visible false  :width 1  :x 1  :y 4}
+                    :9 {:color "rgba( 102, 0, 153, 1)" :visible false  :width 1  :x 1 :y 5}
+                    :2 {:color "rgba( 128, 128, 128, 1)" :visible false  :width 1  :x 4  :y 1}
+                    :5 {:color "rgba( 0, 155, 0, 1)" :visible true :width 1 :x 1 :y 1}
+                    :3 {:color "rgba( 160, 107, 0, 1)" :visible false  :width 1  :x 3 :y 1}
+                    :6 {:color "rgba( 0, 153, 101, 1)" :visible true  :width 1  :x 1  :y 2}}
+         :symbol "NasdaqNM:AMZN"
+         :showLabels true
+         :arcs {:10 {:color "rgba( 0, 0, 153, 1)" :visible true :width 1 :x 5 :y 1}
+                :0 {:color "rgba( 160, 107, 0, 1)" :visible true  :width 1  :x 1  :y 0}
+                :4 {:color "rgba( 105, 158, 0, 1)" :visible true  :width 1  :x 2  :y 1}
+                :7 {:color "rgba( 0, 153, 101, 1)" :visible true  :width 1  :x 4  :y 0}
+                :1 {:color "rgba( 160, 107, 0, 1)" :visible true  :width 1  :x 1 :y 1}
+                :8 {:color "rgba( 0, 153, 101, 1)"  :visible true  :width 1 :x 4  :y 1}
+                :9 {:color "rgba( 0, 0, 153, 1)" :visible true  :width 1  :x 5 :y 0}
+                :2 {:color "rgba( 160, 107, 0, 1)" :visible true :width 1  :x 1.5  :y 0}
+                :5 {:color "rgba( 0, 155, 0, 1)"  :visible true :width 1 :x 3  :y 0}
+                :3 {:color "rgba( 105, 158, 0, 1)" :visible true :width 1 :x 2 :y 0}
+                :6 {:color "rgba( 0, 155, 0, 1)" :visible true :width 1 :x 3 :y 1}}
+         :clonable true
+         :levels {:0 [1 "rgba( 128, 128, 128, 1)" true]
+                  :1 [1 "rgba( 160, 107, 0, 1)" true]
+                  :2 [1 "rgba( 105, 158, 0, 1)" true]
+                  :3 [1 "rgba( 0, 155, 0, 1)" true]
+                  :4 [1 "rgba( 0, 153, 101, 1)" true]
+                  :5 [1 "rgba( 128, 128, 128, 1)" true]}
+         :scaleRatio 737.9710852623244
+         :lastUpdateTime 1636584460200
+         :interval "D"
+         :visible true
+         :frozen false
+         :reverse false}
+ :points [{:time_t a-t :offset 0 :price a-p}
+          {:time_t b-t :offset 0 :price b-p}]
+ :zorder 3
+ :linkKey "tgEoPNzjMxMh"
+ :ownerSource "pOQ6pA"
+ :version 2})
+
+
 
 
 (comment
@@ -397,11 +457,15 @@
                                          (source-trendline {:symbol "NYSE:APA"
                                                             :a-p 48.20  :a-t 1519223400
                                                             :b-p 35.88060448358687  :b-t 1521725400})
-                                         ;source-gann
+                                         (source-gann
+                                          {:symbol "NasdaqNM:AMZN"
+                                           :a-p 1517.0  :a-t 1511879400
+                                           :b-p 1794.0  :b-t 1515076200})
                                          ;source-pitchfork
                                          ])])])
 
        (save-chart 77 77 888))
+  
   
 
 
