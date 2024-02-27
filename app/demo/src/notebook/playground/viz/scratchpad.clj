53ef57a395b91058caabcd6e4b2d8e754210058c
awb99 <awb99@gmail.com>
2024-02-27 13:13:46 -0500

    ta.viz render render-spec working

--- app/demo/src/notebook/playground/viz/scratchpad.clj
+++ app/demo/src/notebook/playground/viz/scratchpad.clj
@@ -44,38 +44,17 @@
      {:volume "column"}]
     [{:volume "column"}]))
 
+(if algo-charts
+    algo-charts
+    (default-algo-chart-spec ds-algo))
 
- [ta.viz.study-highchart :refer [study-highchart] :as hc]
+(if axes-spec
+    axes-spec
+   (if (has-col? ds-study :trade)
+       [{:trade "flags"}
+        {:volume "column"}]
+       [{:volume "column"}]))
 
-(defn highchart [{:keys [ds-algo algo-charts]}]
-  (let [axes-spec (if algo-charts
-                    algo-charts
-                    (default-algo-chart-spec ds-algo))]
-    (println "highchart axes spec:" axes-spec)
-    (-> (study-highchart ds-algo nil); axes-spec)
-        second)))
-
-(defn highchart [ds-study axes-spec]
-  (let [axes-spec (if axes-spec
-                    axes-spec
-                    (if (has-col? ds-study :trade)
-                      [{:trade "flags"}
-                       {:volume "column"}]
-                      [{:volume "column"}]))]
-    (println "highchart axes spec:" axes-spec)
-    (-> (study-highchart ds-study axes-spec)
-        second)))
-
-
-(defn tap-highchart [data]
-  (let [data (highchart data)
-        ;data (assoc data :chart {:height 600 :width 1200})
-        ]
-    (tap> ^{:render-fn 'ui.highcharts/highstock}
-     {:data data
-      :box :lg})))
-
-   ; (require '[ta.viz.study-highchart :refer [ds-epoch series-flags]])
   #_(-> (algo-backtest "buy-hold s&p")
       ;keys
         :backtest
