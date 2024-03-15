7ed86b721fff47e597c391eec70e1a01bb276e50
awb99 <awb99@gmail.com>
2024-03-14 20:17:32 -0500

    new ci script

---
+++ build.clj
@@ -0,0 +1,31 @@
+(ns build
+  (:require
+   [babashka.fs :as fs]
+   [clojure.java.io :as io]
+   [clojure.string :as str]
+   [clojure.tools.build.api :as b]
+   [org.corfield.build :as bb] ; https://github.com/seancorfield/build-clj
+   [deps-deploy.deps-deploy :as dd]))
+
+
+(def lib 'org.pink-gorilla/ta)
+(def version (format "0.3.%s" (b/git-count-revs nil)))
+
+(defn jar [opts]
+  (-> opts
+      (assoc :lib lib
+             :version version
+             :src-pom "pom-template.xml"
+             :transitive true)
+      ;(bb/run-tests)
+      ;(bb/clean)
+      (bb/jar)))
+
+
+(defn deploy [opts]
+  (println "Deploying to Clojars.")
+  (-> opts
+      (assoc :lib lib
+             :version version)
+      (bb/deploy)))
+
