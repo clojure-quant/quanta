78506c48c7289fe0c3cde2d3aa8aaa51d664a168
wizard50 <wizard50@tuta.io>
2024-10-21 19:31:47 -0300

    cljfmt

--- lib/calendar/src/quanta/calendar/compress.clj
+++ lib/calendar/src/quanta/calendar/compress.clj
@@ -1,9 +1,9 @@
 (ns quanta.calendar.compress
   (:require
-    [tick.core :as t]
-    [tech.v3.datatype :as dtype]
-    [tablecloth.api :as tc]
-    [quanta.calendar.core :refer [current-close]]))
+   [tick.core :as t]
+   [tech.v3.datatype :as dtype]
+   [tablecloth.api :as tc]
+   [quanta.calendar.core :refer [current-close]]))
 
 (defn add-date-group-calendar [ds calendar]
   (let [group-fn (partial current-close calendar)
@@ -14,29 +14,29 @@
 
 (defn compress-ds [grouped-ds]
   (->
-    grouped-ds
-    (tc/group-by [:date-group])
-    (tc/aggregate {:open (fn [ds]
-                           (-> ds :open first))
-                   :high (fn [ds]
-                           (->> ds
-                                :high
-                                (apply max)))
-                   :low (fn [ds]
+   grouped-ds
+   (tc/group-by [:date-group])
+   (tc/aggregate {:open (fn [ds]
+                          (-> ds :open first))
+                  :high (fn [ds]
                           (->> ds
-                               :low
-                               (apply min)))
-                   :close (fn [ds]
-                            (-> ds :close last))
-                   :volume (fn [ds]
-                             (->> ds
-                                  :volume
-                                  (apply +)))
-                   :count (fn [ds]
+                               :high
+                               (apply max)))
+                  :low (fn [ds]
+                         (->> ds
+                              :low
+                              (apply min)))
+                  :close (fn [ds]
+                           (-> ds :close last))
+                  :volume (fn [ds]
                             (->> ds
-                                 :close
-                                 (count)))})
-    (tc/rename-columns {:date-group :date})))
+                                 :volume
+                                 (apply +)))
+                  :count (fn [ds]
+                           (->> ds
+                                :close
+                                (count)))})
+   (tc/rename-columns {:date-group :date})))
 
 (defn compress-to-calendar [ds calendar]
   (->  ds
