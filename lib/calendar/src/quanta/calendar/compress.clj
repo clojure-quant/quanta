455e6c466c0a91a98ca551c669c346e3a9fa686b
wizard50 <wizard50@tuta.io>
2024-10-21 19:30:48 -0300

    convert dtz to instant for calendar compress

--- lib/calendar/src/quanta/calendar/compress.clj
+++ lib/calendar/src/quanta/calendar/compress.clj
@@ -1,3 +1,45 @@
-(ns quanta.calendar.compress)
+(ns quanta.calendar.compress
+  (:require
+    [tick.core :as t]
+    [tech.v3.datatype :as dtype]
+    [tablecloth.api :as tc]
+    [quanta.calendar.core :refer [current-close]]))
 
-(defn compress-to-calendar [ds calendar])
+(defn add-date-group-calendar [ds calendar]
+  (let [group-fn (partial current-close calendar)
+        date-group-col (dtype/emap group-fn :zoned-date-time (:date ds))]
+    (tc/add-column ds :date-group date-group-col)))
+
+;; COMPRESS
+
+(defn compress-ds [grouped-ds]
+  (->
+    grouped-ds
+    (tc/group-by [:date-group])
+    (tc/aggregate {:open (fn [ds]
+                           (-> ds :open first))
+                   :high (fn [ds]
+                           (->> ds
+                                :high
+                                (apply max)))
+                   :low (fn [ds]
+                          (->> ds
+                               :low
+                               (apply min)))
+                   :close (fn [ds]
+                            (-> ds :close last))
+                   :volume (fn [ds]
+                             (->> ds
+                                  :volume
+                                  (apply +)))
+                   :count (fn [ds]
+                            (->> ds
+                                 :close
+                                 (count)))})
+    (tc/rename-columns {:date-group :date})))
+
+(defn compress-to-calendar [ds calendar]
+  (->  ds
+       (add-date-group-calendar calendar)
+       compress-ds
+       (tc/map-columns ds :date t/instant)))
